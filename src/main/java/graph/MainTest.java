2f07bac6469e45c801d61be8f4999a053446be17
Qin DI <teki0204@yahoo.co.jp>
2018-10-11 11:23:08 +0900

    painting

--- src/main/java/graph/MainTest.java
+++ src/main/java/graph/MainTest.java
@@ -9,27 +9,19 @@ public class MainTest extends Application {
   public static void main(String[] args) {
     Application.launch(args);
   }
-
   @Override
   public void start(Stage primaryStage) {
     Pane pane = new Pane();
     TimerCircle circle = new TimerCircle();
-    TimerTri triangle = new TimerTri();
     Thread c = new Thread(circle);
-    Thread t = new Thread(triangle);
-
-/*Threadのプログラムを起動させる*/
     c.start();
-    t.start();
-
-/*表示エリアを設置する*/
     circle.Set(primaryStage, pane);
-    triangle.Set(primaryStage, pane);
-
-/*描画のプログラムを起動させる*/
     circle.init(primaryStage, pane);
+    TimerTri triangle = new TimerTri();
+    Thread t = new Thread(triangle);
+    t.start();
+    triangle.Set(primaryStage, pane);
     triangle.init(primaryStage, pane);
-
     primaryStage.setScene(new Scene(pane, 400, 400));
     primaryStage.show();
   }
